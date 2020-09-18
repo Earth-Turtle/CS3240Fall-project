d5bc0d579c43457cdafbaf45cc256041e7690f04
Hasan Mukati <mukati.hasan@gmail.com>
2020-09-18 13:37:12 -0400

    First Commit
    Simple home page

---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import url
+from home import views 
+
+
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+
+    
+]

9b537c3e5d479aab0aff20e6343c84c0f93a688b
EarthTurtle <43360323+EarthTurtleDerp@users.noreply.github.com>
2020-10-05 19:01:14 -0400

    completed up to step 7 of the tutorial, working on Google's end now

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from home import views
 
 
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('accounts/', views.accounts_view, name='accounts')
-
+    path('', TemplateView.as_view(template_name='home/index.html')),
+    path('accounts/', views.accounts_view, name='accounts'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     
 ]

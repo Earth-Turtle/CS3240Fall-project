dd4a0d75a41a5f8c18cbb0cde0e002798a2adfb2
Hasan Mukati <mukati.hasan@gmail.com>
2020-10-18 12:25:45 -0400

    Added Templates
    Templates, comments & categories.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    publish = models.DateTimeField(default=timezone.now)
+    slug = models.SlugField(null=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
+    
--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic
+
+from .models import Category
+from .models import Post
+from .models import Comment
+
 
 # Create your views here.
 
@@ -10,3 +15,49 @@ class IndexView(generic.TemplateView):
 
 def accounts_view(request):
     return render(request, "accounts.html")
+
+
+def categories(request):
+    catgories = list(Category.objects.all())
+
+    return render(request,"categories.html",{'catgories':catgories})
+
+
+def singleCategory(request, categorySlug):
+    getCategory = Category.objects.get(slug=categorySlug)
+    posts = list(Post.objects.filter(category=getCategory))
+
+
+    return render(request,"posts.html",{'posts':posts})
+
+
+def singleCategory(request, categorySlug):
+    getCategory = Category.objects.get(slug=categorySlug)
+    posts = list(Post.objects.filter(category=getCategory))
+
+
+    return render(request,"posts.html",{'posts':posts})
+
+
+def comment(request, categorySlug, postSlug):
+    getCategory = Category.objects.get(slug=categorySlug)
+    post = Post.objects.get(slug=postSlug)
+    comments = list(Comment.objects.filter(post=post))
+
+    return render(request,"comment.html",{'post':post, 'comments':comments})
+
+
+def postComment(request):
+    dataIn = request.POST.copy()
+    post = dataIn['post']
+    name = dataIn['name']
+    comments = dataIn['comments']
+
+
+    commentModel = Comment()
+    commentModel.post = Post.objects.get(id=post)
+    commentModel.author = name
+    commentModel.text = comments
+    commentModel.save()
+
+    return redirect('/categories/')

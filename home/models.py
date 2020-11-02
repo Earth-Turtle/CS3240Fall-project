d2ea6182caeadd2b179053e2b3996b4e0610c4b3
EarthTurtle <43360323+EarthTurtleDerp@users.noreply.github.com>
2020-11-02 18:56:51 -0500

    reverted comment to not use User as foreign key

--- home/models.py
+++ home/models.py
@@ -34,7 +34,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
 

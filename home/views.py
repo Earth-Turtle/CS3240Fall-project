02ae2dbb5823cdaf56f2f0c3ee6459d93c7fc210
DJ Exel <dje3ac@virginia.edu>
2020-11-02 22:34:30 -0500

    fiddling around with profile page, creates user profile if not already defined, working phone number change

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.views import generic
 
+
 from .models import Category
 from .models import Post
 from .models import Comment, UserProfile
@@ -54,25 +55,36 @@ def postComment(request):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
     
 def myProfile(request):
-
     data = {}
-    data['email'] = request.user.email
-    data['first_name'] = request.user.first_name
-    data['last_name'] = request.user.last_name
+    try:
+        profile = UserProfile.objects.get(user=request.user)
+        data['email'] = profile.user.email
+        data['first_name'] = profile.user.first_name
+        data['last_name'] = profile.user.last_name
+        data['phone'] = profile.phone
 
+    except: #NOT SECURE: FIND BETTER WAY TO DO THIS
+        data['email'] = request.user.email
+        data['first_name'] = request.user.first_name
+        data['last_name'] = request.user.last_name
     return render(request,"my-profile.html",data)
 
 
 def myProfileAction(request):
     dataIn = request.POST.copy()
-    print(request)
-    print(dataIn)
-    userModel = User.objects.get(email=dataIn['email'])
-    userModel.first_name = dataIn['first_name']
-    userModel.last_name = dataIn['last_name']
-    userModel.email = dataIn['email']
-    userModel.phone = dataIn['phone']
-    userModel.save()
+
+    try:
+        new_profile = UserProfile.objects.get(user=request.user)
+    except:
+        new_profile = UserProfile()
+        new_profile.user = User.objects.get(id=request.user.id)
+
+    #NOTE: the following will only save phone number, not the other stuff; will figure out how to do that later
+    new_profile.user.first_name = dataIn['first_name']
+    new_profile.user.last_name = dataIn['last_name']
+    new_profile.user.email = dataIn['email']
+    new_profile.phone = dataIn['phone']
+    new_profile.save()
     ###update here
 
     return redirect('/')
